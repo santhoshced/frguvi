76e28d01601e1e7c790d5c11273dc45d57fb6c3d
santhoshced <santhoshcedric777@gmail.com>
2017-08-25 16:28:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the sentence");
+            string s = Console.ReadLine();
+            string[] w = s.Split(' ');
+            for (int i = 0; i < w.Length; i++)
+            {
+                Console.WriteLine(w[i]);
+            }
+
+            for (int i = 0; i < w.Length; i++)
+            {
+                for (int j = i+1; j < w.Length; j++)
+                {
+                    int c=string.Compare(w[i],w[j]);
+                    if (c == 1)
+                    {
+                        string temp = string.Copy(w[j]);
+                        w[j] = w[i];
+                        w[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("after sort");
+            for (int i = 0; i < w.Length; i++)
+            {
+               
+                    Console.WriteLine(w[i]);
+               
+            }
+
+                Console.ReadLine();
+        
+        }
+    }
+}
